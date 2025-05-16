6e954554485bf059881211bc090bd487fa0c75bd
Hugo Oyola <hugo212h@gmail.com>
2025-05-15 23:04:28 -0500

    Agregar funcionalidad de tabs en el componente ContenidoPrincipal y crear componente Historial

--- src/app/page/main/components/contenido-principal/contenido-principal.component.ts
+++ src/app/page/main/components/contenido-principal/contenido-principal.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RegistroComponent } from "./registro/registro.component";
+import { ListadoComponent } from "./listado/listado.component";
+import { GestionComponent } from "./gestion/gestion.component";
+import { HistorialComponent } from "./historial/historial.component";
 
 @Component({
   selector: 'app-contenido-principal',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RegistroComponent, ListadoComponent, GestionComponent, HistorialComponent],
   templateUrl: './contenido-principal.component.html',
   styleUrl: './contenido-principal.component.scss'
 })
 export class ContenidoPrincipalComponent {
+  // Tab activo por defecto
   public activeTab: 'registro' | 'listado' | 'gestion' | 'historial' = 'registro';
 
+  // Método para cambiar el tab activo
+  cambiarTab(tab: 'registro' | 'listado' | 'gestion' | 'historial'): void {
+    this.activeTab = tab;
+  }
 }
